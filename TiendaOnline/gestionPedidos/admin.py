97724201d61b357c560efe4e52e9943423cb4d5d
JordanATM <112110782+JordanATM@users.noreply.github.com>
2023-06-08 14:21:32 -0400

    Agregar modelos al panel de administración

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from gestionPedidos import models
 
 # Register your models here.
+
+admin.site.register(models.Clientes)
+admin.site.register(models.Articulos)
+admin.site.register(models.Pedidos)
--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -5,13 +5,16 @@ from django.db import models
 class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     telefono = models.CharField(max_length=15)
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=30)
     precio = models.IntegerField()
+
+    def __str__(self):
+        return "Articulo: %s | sección: %s | precio: %s"%(self.nombre, self.seccion, self.precio)
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
