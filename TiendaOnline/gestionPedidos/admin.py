3c841006cf311a69f213aafd0b2582072d4b1957
JordanATM <112110782+JordanATM@users.noreply.github.com>
2023-06-09 14:15:16 -0400

    Crear formulario en html

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -3,6 +3,21 @@ from gestionPedidos import models
 
 # Register your models here.
 
-admin.site.register(models.Clientes)
-admin.site.register(models.Articulos)
-admin.site.register(models.Pedidos)
+class Clientes_admin(admin.ModelAdmin):
+    list_display=("nombre","direccion","email","telefono")
+    search_fields=("nombre","email","telefono",)
+
+class Articulos_admin(admin.ModelAdmin):
+    list_display=("nombre","seccion","precio")
+    search_fields=("nombre","seccion","precio",)
+    list_filter=("seccion",)
+
+class Pedido_admin(admin.ModelAdmin):
+    list_display=("numero","fecha","entregado")
+    search_fields=("numero","fecha","entregado",)
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
+admin.site.register(models.Clientes, Clientes_admin)
+admin.site.register(models.Articulos,Articulos_admin)
+admin.site.register(models.Pedidos,Pedido_admin)
